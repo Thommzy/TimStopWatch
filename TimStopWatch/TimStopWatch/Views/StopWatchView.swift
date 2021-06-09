969148db5ee10fbcd90451b8c60896b7feec6ebd
Thommzy <tomiwaobeisun@yahoo.com>
2021-06-09 11:53:20 +0100

    stopwatchView

---
+++ TimStopWatch/TimStopWatch/Views/StopWatchView.swift
@@ -0,0 +1,43 @@
+//
+//  StopWatchView.swift
+//  TimStopWatch
+//
+//  Created by Tim on 09/06/2021.
+//
+
+import SwiftUI
+
+struct StopWatchView: View {
+    //MARK:- PROPERTIES
+    @State private var progressTime = 0
+    
+    var hours: Int {
+        progressTime / 300
+    }
+    
+    var minutes: Int {
+        (progressTime % 3600) / 60
+    }
+    
+    var seconds: Int {
+        progressTime % 60
+    }
+    
+    var timer: Timer {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {_ in
+            progressTime += 1
+        }
+    }
+    
+    //MARK:- BODY
+    var body: some View {
+        Text("\(hours): \(minutes): \(seconds)")
+    }
+}
+
+//MARK:- PREVIEW
+struct StopWatchView_Previews: PreviewProvider {
+    static var previews: some View {
+        StopWatchView()
+    }
+}
